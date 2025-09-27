b623e544b92c34f5983a3006481d4c08e8c555d8
Gary Velasquez <garyjvc000@gmail.com>
2025-09-26 21:41:22 -0600

    polishing attributes, methods and classes

--- Simple.py
+++ Simple.py
@@ -8,49 +8,44 @@ Autor: (Gary)
 Fecha: 24/09/2025
 """
 def main():
-	print("Usa como variables n ")
-	prop1= input("Ingrese tu primera proposicion")
-	comp = input("Ingrese el comparador (=)")
-	prop2= input("Ingrese tu segunda proposicion")
+    print("Usa como variables n ")
+    prop1= input("Ingrese tu primera proposicion")
+    comp = input("Ingrese el comparador (=)")
+    prop2= input("Ingrese tu segunda proposicion")
+    var = input("Ingresa tu variable a utilizar en la proposicion")
+
+    enun = Proposicion(prop1, comp, prop2, var)
+
 
 #caso base
 
 class Proposicion:
-    def __init__(self, prop1, comp, prop2):
+    def __init__(self, prop1, comp, prop2, var):
         self.prop1 = prop1
         self.comp = comp
         self.prop2 = prop2
-
-    def evaluar(self):
-        if self.comp == "=":
-            return self.prop1 == self.prop2
-        else:
-            raise ValueError("Comparador no válido")
-
-class lecturaDeProposicion(Proposicion):
-    def __init__(self, prop1, comp, prop2):
-        super().__init__(prop1, comp, prop2)
-
-    def evaluar(self):
-        return super().evaluar()
+        self.var = var
     
 class casoBase(Proposicion):
-    def __init__(self, prop1, comp, prop2):
-        super().__init__(prop1, comp, prop2)
+    def __init__(self, prop1, comp, prop2, var):
+        super().__init__(prop1, comp, prop2,var)
 
     def evaluar(self):
-        return super().evaluar()
+        pass
     
 class hipotesisDeInduccion(Proposicion):
-    def __init__(self, prop1, comp, prop2):
-        super().__init__(prop1, comp, prop2)
+    def __init__(self, prop1, comp, prop2,var, varN1):
+        self.varN1 = varN1
+        super().__init__(prop1, comp, prop2,var)
 
-    def evaluar(self):
-        return super().evaluar()
+    def Proposicion(self):
+        pass
     
-class casoInductivo(Proposicion):
-    def __init__(self, prop1, comp, prop2):
-        super().__init__(prop1, comp, prop2)
+class pasoInductivo(Proposicion,hipotesisDeInduccion):
+    def __init__(self, prop1, comp, prop2, varN1):
+        super().__init__(prop1, comp, prop2, varN1)
 
     def evaluar(self):
-        return super().evaluar()
+        pass
+    def comparar(self):
+        pass

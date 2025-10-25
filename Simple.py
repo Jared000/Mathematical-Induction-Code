3c0daa00843b5de9b9e1b4e9df77a9a99668ace6
Gary Velasquez <garyjvc000@gmail.com>
2025-10-24 23:40:41 -0600

    Progess in my base case!!!

--- Simple.py
+++ Simple.py
@@ -7,16 +7,6 @@ Descripción: Archivo de ejemplo en Python.
 Autor: (Gary)
 Fecha: 24/09/2025
 """
-def main():
-    print("Usa como variables n ")
-    prop1= input("Ingrese tu primera proposicion")
-    comp = input("Ingrese el comparador (=)")
-    prop2= input("Ingrese tu segunda proposicion")
-    var = input("Ingresa tu variable a utilizar en la proposicion")
-
-    enun = Proposicion(prop1, comp, prop2, var)
-
-
 #caso base
 
 class Proposicion:
@@ -26,22 +16,48 @@ class Proposicion:
         self.prop2 = prop2
         self.var = var
     
+    def __str__(self):
+        return f"{self.prop1} {self.prop2}, con variable {self.var}"
+    
 class casoBase(Proposicion):
     def __init__(self, prop1, comp, prop2, var):
         super().__init__(prop1, comp, prop2,var)
 
-    def evaluar(self):
-        pass
+    def lecture(self):
+        lec1 =[] 
+        for i in self.prop1:
+            lec1.append("1" if i == "n" else i)
+
+        lec2 = []
+        for i in self.prop2:
+            lec2.append("1" if i == "n" else i)
+
+        self.evaluar(lec1, lec2)
+        
     
+    def evaluar(self,lec1,lec2):
+            exp1 = "".join(lec1)
+            exp2 = "".join(lec2)
+
+
+            ec1 = eval(exp1)
+            ec2 = eval(exp2)
+
+            if ec1 == ec2:
+                print("Caso Base confirmado !!!")
+            else:
+                print("Demostracion no valida")
+
 class hipotesisDeInduccion(Proposicion):
     def __init__(self, prop1, comp, prop2,var, varN1):
         self.varN1 = varN1
         super().__init__(prop1, comp, prop2,var)
 
+
     def Proposicion(self):
         pass
     
-class pasoInductivo(Proposicion,hipotesisDeInduccion):
+class pasoInductivo(hipotesisDeInduccion):
     def __init__(self, prop1, comp, prop2, varN1):
         super().__init__(prop1, comp, prop2, varN1)
 
@@ -49,3 +65,24 @@ class pasoInductivo(Proposicion,hipotesisDeInduccion):
         pass
     def comparar(self):
         pass
+
+
+
+
+def main():
+    print("Usa como variables n ")
+    prop1= input("Ingrese tu primera proposicion ")
+    comp = input("Ingrese el comparador (=) ")
+    prop2= input("Ingrese tu segunda proposicion ")
+    var = input("Ingresa tu variable a utilizar en la proposicion ")
+
+    enun = Proposicion(prop1, comp, prop2, var)
+    print("\n Proposicion general:", enun)
+
+    caseB = casoBase(prop1, comp, prop2,var)
+    caseB.lecture()
+    #caseB.evaluar()
+
+if __name__ == "__main__":
+    main()
+
